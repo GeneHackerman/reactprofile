f376120fba03c4ba0242bb8d11ec16e58d47cef4
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-05-08 21:57:39 -0400

    editing about page

--- react-profile/src/components/About/index.js
+++ react-profile/src/components/About/index.js
@@ -8,7 +8,12 @@ function About() {
             <img src={coverImage} className="my-2" style={{ width: "75%" }} alt="cover" />
             <div className="my-2">
             <p>
-                Welcome to my portfolio page using REACT.js! 
+                Hello, my name is Rafael Arias and welcome to my portfolio page created with REACT.js! 
+            </p>
+            <p>
+                I am currently a bootcamp student and will be graduating on May 26th, 2022. 
+                After the bootcamp is over, I am hoping to get my foot through the door within the industry as a full-stack engineer.
+                Thanks to the bootcmap, I collaborated with other students to create front-end and full-stack projects. 
             </p>
             </div>
 
